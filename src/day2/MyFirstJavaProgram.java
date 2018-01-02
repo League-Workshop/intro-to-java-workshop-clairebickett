681c003dfe84d0eafbe5f819cdecab84c5336dbc
clairebickett <league@WTS5.attlocal.net>
2018-01-02 14:58:07 -0800

    Day 1 Workshop

--- src/day2/MyFirstJavaProgram.java
+++ src/day2/MyFirstJavaProgram.java
@@ -6,10 +6,18 @@ import org.jointheleague.graphical.robot.Robot;
 public class MyFirstJavaProgram {
 	
 	public static void main(String[] args) {
+
 		
 		// START HERE
-	
-		
-		
+	System.out.println("Hello World!");
+	Robot carl	= new Robot(); 
+	carl.penDown();
+	carl.setSpeed(100);
+	for(int i = 0; i < 4; i++) {
+	carl.setRandomPenColor();
+	carl.move(200);
+    carl.turn(90);} 
+    carl.sparkle();
 	}
 }
+
